6928e5348a59a6cbd7dc906a0fa45c4c18e1a347
ngacharich2014 <ngacharich2014@fau.edu>
2021-01-19 15:08:34 -0500

    fix h2

--- src/components/Project.js
+++ src/components/Project.js
@@ -26,7 +26,7 @@ export default function Project() {
         <main className="bg-purple-100 min-h-screen p-12">
             <section className="container mx-auto">
                 <h1 className="text-5xl flex jusify-center cursive">My Projects</h1>
-                <h2 className="text-lg text-gray-600 flex justify-center mb-12"></h2>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page!</h2>
                 <section className="grid grid-cols-2 gap-8">
                     {projectData && projectData.map((project, index) =>(
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
